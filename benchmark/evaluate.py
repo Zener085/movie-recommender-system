77fcd3e1349c21bec7aa1799e5212e7b0ceac865
Zener_085 <57841939+Zener085@users.noreply.github.com>
2023-12-03 19:06:42 +0300

    fine-tuning and evaluation fix
    Fixed script for evaluation
    Added notebook with fine-tunning

--- benchmark/evaluate.py
+++ benchmark/evaluate.py
@@ -14,20 +14,22 @@ from models.model import Recommender
 
 
 def _generate_user(__id: int, __data: pd.DataFrame) -> List:
-    _user = df[df["UserId"] == _user_id][["Age", "Gender", "Occupation"]].iloc[0].tolist()
+    _user = __data[__data["UserId"] == __id][["Age", "Gender", "Occupation"]].iloc[0].tolist()
     _user.append([])
-    for _title, _rating in df[df["UserId"] == _user_id][["Title", "Rating"]].values:
+    for _title, _rating in __data[__data["UserId"] == __id][["Title", "Rating"]].values:
         if _rating > 3:
             _user[-1].append((str(_title), int(_rating)))
 
     return _user
 
 
-def _generate_test(__data: pd.DataFrame, _total: int = 1000) -> Tuple[List, List]:
-    _user_ids = [np.random.randint(1, __data.shape[0]) for _ in range(_total)]
+def _generate_test(__data: pd.DataFrame, __total: int = 1000) -> Tuple[List, List]:
     _users, _likes = [], []
-    for _user_id in _user_ids:
-        _user = _generate_user(_user_id, _data)
+    while len(_users) < __total:
+        _user_id = np.random.choice(__data["UserId"].unique())
+        _user = _generate_user(_user_id, __data)
+        if not len(_user[-1]):
+            continue
         _likes.append(_user[-1].pop()[0])
         _users.append(_user)
     return _users, _likes
@@ -39,14 +41,14 @@ def evaluate(recommender: Recommender, __data: pd.DataFrame):
     _ideal = [True] * _total
     _users, _likes = _generate_test(__data, _total)
     for _user, _like in zip(_users, _likes):
-        _movies = zip(*recommender.suggest(*_user))[0]
+        _movies = list(map(lambda x: x[0], recommender.suggest(*_user)))
         _pred.append(_like in _movies)
 
-    _roc, _acc, _f1 = roc_auc_score(_ideal, _pred), accuracy_score(_ideal, _pred), _ndcg_score(_ideal, _pred)
+    _acc, _f1 = accuracy_score(_ideal, _pred), f1_score(_ideal, _pred)
 
-    print(f"ROC AUC score: {_roc}\nAccuracy: {_acc * 100}%\n F1: {_ndcg}")
+    print(f"Accuracy: {_acc * 100}%\n F1: {_f1}")
 
 
 if __name__ == "__main__":
     data = pd.read_csv("data/test_ratings.csv")
-    evaluate(Recommender(5, data), data)
+    evaluate(Recommender(16, data, leaf_size=64), data)
